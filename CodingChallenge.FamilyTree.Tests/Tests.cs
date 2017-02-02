9a5aa6f549dbe06a621cff4fa80b50530ddfc22f
Nathan Gonzalez <ncgonz@gmail.com>
2017-02-01 20:20:23 -0600

    Update Tests.cs

--- CodingChallenge.FamilyTree.Tests/Tests.cs
+++ CodingChallenge.FamilyTree.Tests/Tests.cs
@@ -7,7 +7,7 @@ namespace CodingChallenge.FamilyTree.Tests
     public class TreeTests
     {
         
-        [Theory]
+        [Fact]
         public void if_the_person_exists_in_tree_the_result_should_be_january()
         {
             var tree = FamilyTreeGenerator.Make();
@@ -15,7 +15,7 @@ namespace CodingChallenge.FamilyTree.Tests
             result.ShouldEqual("January");
         }
            
-        [Theory]
+        [Fact]
         public void if__the_person_exists_at_the_top_tree_the_result_should_be_may()
         {
             var tree = FamilyTreeGenerator.Make();
@@ -23,7 +23,7 @@ namespace CodingChallenge.FamilyTree.Tests
             result.ShouldEqual("May");
         }
 
-        [Theory]
+        [Fact]
         public void if_the_person_does_not_exist_in_the_tree_the_result_should_be_empty()
         {
             var tree = FamilyTreeGenerator.Make();
