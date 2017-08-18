4b1a84c00928b6a1796c0706fe6dbf150477b5ed
blairvansant <blairbot@gmail.com>
2017-08-18 09:29:15 -0500

    familyTree passes tests

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 namespace CodingChallenge.FamilyTree
 {
     public class Solution
     {
         public string GetBirthMonth(Person person, string descendantName)
+        {    
+            var descendant = findDescendant(person, descendantName);
+
+            return descendant?.Birthday.ToString("MMMM") ?? string.Empty;
+        }
+
+        private Person findDescendant(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            return person.Name == descendantName ? person : person.Descendants.Select(i => findDescendant(i, descendantName)).FirstOrDefault(result => result != null);
         }
     }
 }
+
+
+
+
